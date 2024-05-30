6c3389b4a2f63c5c5a616fffc7624341a1baf86a
jmchargue54 <jmchargue54@gmail.com>
2024-05-30 12:46:57 -0600

    gpa

---
+++ ponders/arraymethods/scripts/array.js
@@ -0,0 +1,16 @@
+//  arrays.js
+const steps = ["one", "two", "three"];
+
+steps.forEach((step)) = > {
+  document.querySelector("#myList").innerHTML += `<li>${step}</li>`;
+}
+
+// steps.map((let 1 = 0; i < steps.length; i++) => {
+//   document.querySelector("#myList").innerHTML += `<li>${step}</li>`;
+// });
+
+// const listTemplate(step) {
+//   return //the html string made from step
+// }
+// const stepsHtml = // use map to convert the list from strings to HTML
+// document.querySelector("#myList").innerHTML = // set the innerHTML
---
+++ ponders/gpacalculator/gpa.js
@@ -0,0 +1,56 @@
+document.querySelector('button').addEventListener('click', () => {
+  
+  function getGrades(inputSelector) {
+    // get grades from the input box
+    const grades = document.querySelector('input').value;
+    // split them into an array (String.split(','))
+    const gradeArray = grades.split(',');
+    // clean up any extra spaces, and make the grades all uppercase. (Array.map())
+    const cleanGrades = gradeArray.map((grade) => grade.trim().toUpperCase());
+    // return grades
+    return cleanGrades;
+  }
+  
+  function lookupGrade(grade) {
+    // converts the letter grade to it's GPA point value and returns it
+    let points = 0;
+    if (grade === "A") {
+      points = 4;
+    } else if (grade === "B") {
+      points = 3;
+    } else if (grade === "C") {
+      points = 2;
+    } else if (grades === "D") {
+      points = 1;
+    }
+    return points;
+  }
+  
+  function calculateGpa(grades) {
+    // gets a list of grades passed in
+    // convert the letter grades to gpa points
+    const gradePoints = grades.map((grade) => lookupGrade(grade));
+    // calculates the GPA
+    const gpa = gradePoints.reduce((total, num) => total + num)
+    // return the GPA
+    return gpa.toFixed(2)
+  }
+  
+  function outputGpa(gpa, selector) {
+    // takes a gpa value and displays it in the HTML in the element identified by the selector
+    const outputElement = document.querySelector(selector);
+    outputElement.innerText = gpa;
+  }
+  
+  function clickHandler() {
+    event.preventDefault();
+    // when the button in our html is clicked:
+    // get the grades entered into the input
+    const grades = getGrades("#grades");
+    // calculate the gpa from the grades entered
+    const gpa = calculateGpa(grades);
+    // display the gpa
+    outputGpa(gpa, "#output");
+  }
+  document.querySelector("#submitButton").addEventListener("click", clickHandler);
+})
